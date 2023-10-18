7177bc1dc349ab5136aae2061db5a1a9d7b7a987
ipachecoc1600@alumno.ipn.mx <ipachecoc1600@alumno.ipn.mx>
2023-10-18 12:00:40 -0600

    Se agrego el código para generar los n-digitos de pi

---
+++ utils.js
@@ -0,0 +1,69 @@
+function calcularPiChudnovsky(n) {
+    // Definir la precisión deseada (n + 1 dígitos)
+    // Ajustar la precisión para obtener n + 1 dígitos
+    const precision = Math.ceil((n + 1) * Math.log10(10) / Math.log10(16));
+    BigNumber.config({ DECIMAL_PLACES: precision });
+    
+    // Inicializar variables con BigInt.js
+    let suma = new BigNumber(0);
+
+    
+    // Calcular la serie de Chudnovsky
+    for (let k = 0; k < precision; k++) {
+      const numerador = factorial(BigNumber(6).times(BigNumber(k))).times(545140134n * BigInt(k) + 13591409n);
+      const denominador = factorial(BigNumber(3).times(BigNumber(k))).times(factorial(BigNumber(k)).pow(BigNumber(3))).times("-262537412640768000").pow(BigNumber(k));
+      
+      suma = suma.plus(numerador.dividedBy(denominador));
+    }
+    
+    // Calcular el valor de Pi
+    const pi = BigNumber(426880).times(BigNumber(10005).sqrt()).dividedBy(suma);
+    
+    // Devolver Pi con "n" dígitos
+    return pi.toString().slice(0, n + 1);
+  }
+  
+  function factorial(n) {
+    if (n.lte(1)) return BigNumber(1);
+    let result = BigNumber(1);
+    for (let i = BigNumber(2); i.lte(n); i = i.plus(1)) {
+      result = result.times(i);
+    }
+    return result;
+  }
+  
+  function sqrt(n) {
+    return BigNumber(n).sqrt();
+  }
+  
+const sleep = (millis) => {
+    return new Promise(resolve => setTimeout(resolve, millis))
+}
+  
+
+  document.addEventListener("DOMContentLoaded", function() {
+
+    const btnIniciar = document.getElementById("iniciar");
+    const inputNumDigits = document.getElementById("lblNumDigits");
+    const txtRest = document.getElementById("txtRes");
+
+    btnIniciar.addEventListener("click", async function() {
+
+        let iter = inputNumDigits.value;
+
+        if( iter == null || iter == undefined || iter == "" || iter <= 0) 
+            iter = 100;
+        
+        for(let i=0; i<=iter; i++){
+            let pi_res = calcularPiChudnovsky(i);
+            txtRest.value = pi_res+"";
+            console.log("Valor de pi para 200 digitos:", i);
+            if (i<30)
+                await sleep(50);
+            else   
+                await sleep(1);
+        }
+        
+    });
+
+  });
